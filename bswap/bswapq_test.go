c4e23ac9553bfc7683e9fba8b59f9b05b3bf3f19
Chris O'Hara <cohara87@gmail.com>
2021-04-22 21:08:25 +1000

    Test varying lengths

--- bswap/bswapq_test.go
+++ bswap/bswapq_test.go
@@ -8,20 +8,21 @@ import (
 )
 
 func TestBSwapQ(t *testing.T) {
-	input := make([]byte, 64*1024)
+	input := make([]byte, 4096)
 	prng := rand.New(rand.NewSource(0))
 	io.ReadFull(prng, input)
 
-	output := make([]byte, 1024)
-	copy(output, input)
-	BSwapQ(output)
+	output := make([]byte, 4096)
 
-	for i := 0; i < len(output); i += 8 {
-		u1 := binary.BigEndian.Uint64(input[i:])
-		u2 := binary.LittleEndian.Uint64(output[i:])
-
-		if u1 != u2 {
-			t.Fatalf("bytes weren't swapped at offset %d: %v / %v", i, u1, u2)
+	for i := 0; i < 4096; i += 8 {
+		copy(output, input)
+		BSwapQ(output[:i])
+		for j := 0; j < i; j += 8 {
+			u1 := binary.BigEndian.Uint64(input[j:])
+			u2 := binary.LittleEndian.Uint64(output[j:])
+			if u1 != u2 {
+				t.Fatalf("bytes weren't swapped at offset %d: %v / %v", i, u1, u2)
+			}
 		}
 	}
 }
