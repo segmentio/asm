2117f5a23278bc3777aeb89f13342e5181d64b2e
Chris O'Hara <cohara87@gmail.com>
2021-05-06 10:21:06 +1000

    Tidy up tests/benchmarks

--- sortedset/dedupe_test.go
+++ sortedset/dedupe_test.go
@@ -9,6 +9,10 @@ import (
 	"testing"
 )
 
+var specializationSizes = []int{16, 32}
+
+var repeatChances = []float64{0, 0.1, 0.5, 1.0}
+
 func TestDedupe(t *testing.T) {
 	for _, test := range []struct {
 		name   string
@@ -46,62 +50,49 @@ func TestDedupe(t *testing.T) {
 			}
 		})
 	}
-}
 
-var repeatChances = []float64{0, 0.1, 0.5, 1.0}
+	// Test the specializations.
+	for _, size := range specializationSizes {
+		t.Run(fmt.Sprintf("size %d, random", size), func(t *testing.T) {
+			const maxCount = 100
+			const iterations = 1000
 
-func TestDedupe16(t *testing.T) {
-	testDedupeSize(t, 16)
-}
+			prng := rand.New(rand.NewSource(0))
 
-func TestDedupe32(t *testing.T) {
-	testDedupeSize(t, 32)
-}
-
-func testDedupeSize(t *testing.T, size int) {
-	const maxCount = 100
-	const iterations = 1000
-
-	prng := rand.New(rand.NewSource(0))
-
-	for i := 0; i < iterations; i++ {
-		count := prng.Intn(maxCount)
-		for _, p := range repeatChances {
-			array, uniques := randomSortedArray(prng, size, count, p)
-			result := Dedupe(array, size)
-			if !reflect.DeepEqual(result, uniques) {
-				t.Fatal("unexpected result")
+			for i := 0; i < iterations; i++ {
+				count := prng.Intn(maxCount)
+				for _, p := range repeatChances {
+					array, uniques := randomSortedArray(prng, size, count, p)
+					result := Dedupe(array, size)
+					if !reflect.DeepEqual(result, uniques) {
+						t.Fatal("unexpected result")
+					}
+				}
 			}
-		}
+		})
 	}
 }
 
-func BenchmarkDedupe16(b *testing.B) {
-	benchmarkDedupeSize(b, 16)
-}
+func BenchmarkDedupe(b *testing.B) {
+	for _, size := range specializationSizes {
+		for _, p := range repeatChances {
+			b.Run(fmt.Sprintf("size %d, with %d%% chance of repeat", size, int(p*100)), func(b *testing.B) {
+				const bytes = 64 * 1024
 
-func BenchmarkDedupe32(b *testing.B) {
-	benchmarkDedupeSize(b, 32)
-}
+				prng := rand.New(rand.NewSource(0))
 
-func benchmarkDedupeSize(b *testing.B, size int) {
-	const bytes = 64 * 1024
+				array, _ := randomSortedArray(prng, size, bytes/size, p)
+				buf := make([]byte, len(array))
 
-	prng := rand.New(rand.NewSource(0))
+				b.SetBytes(bytes)
+				b.ResetTimer()
 
-	for _, p := range repeatChances {
-		b.Run(fmt.Sprintf("%.2f", p), func(b *testing.B) {
-			array, _ := randomSortedArray(prng, size, bytes/size, p)
-			buf := make([]byte, len(array))
-
-			b.SetBytes(bytes)
-			b.ResetTimer()
-
-			for i := 0; i < b.N; i++ {
-				copy(buf, array)
-				Dedupe(buf, size)
-			}
-		})
+				for i := 0; i < b.N; i++ {
+					copy(buf, array)
+					Dedupe(buf, size)
+				}
+			})
+		}
 	}
 }
 
