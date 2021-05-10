02219e4702e9da75e2d56421f49bc6f3e1beb693
Jeremy Larkin <jeremy.larkin@segment.com>
2021-05-10 12:29:53 -0700

    refactor code generation with branches in main

--- ascii/equal_fold_amd64.s
+++ ascii/equal_fold_amd64.s
@@ -5,132 +5,132 @@
 // func EqualFoldString(a string, b string) bool
 // Requires: AVX, AVX2, SSE4.1
 TEXT ·EqualFoldString(SB), NOSPLIT, $0-33
-	MOVQ         a_base+0(FP), AX
-	MOVQ         a_len+8(FP), CX
-	MOVQ         b_base+16(FP), DX
-	CMPQ         CX, b_len+24(FP)
+	MOVQ         a_base+0(FP), CX
+	MOVQ         a_len+8(FP), DX
+	MOVQ         b_base+16(FP), BX
+	CMPQ         DX, b_len+24(FP)
 	JNE          done
-	XORQ         DI, DI
+	XORQ         AX, AX
 	MOVQ         $0xdfdfdfdfdfdfdfdf, SI
 	BTL          $0x08, github·com∕segmentio∕asm∕cpu·X86+0(SB)
 	JCC          eq8
 	PINSRQ       $0x00, SI, X8
 	VPBROADCASTQ X8, Y8
-	CMPQ         CX, $0x80
+	CMPQ         DX, $0x80
 	JNB          eq128
 
 eq64:
-	CMPQ      CX, $0x40
+	CMPQ      DX, $0x40
 	JB        eq32
-	VPAND     (AX)(DI*1), Y8, Y0
-	VPAND     (DX)(DI*1), Y8, Y1
+	VPAND     (CX)(AX*1), Y8, Y0
+	VPAND     (BX)(AX*1), Y8, Y1
 	VPCMPEQB  Y1, Y0, Y0
-	VPAND     32(AX)(DI*1), Y8, Y2
-	VPAND     32(DX)(DI*1), Y8, Y3
+	VPAND     32(CX)(AX*1), Y8, Y2
+	VPAND     32(BX)(AX*1), Y8, Y3
 	VPCMPEQB  Y3, Y2, Y2
 	VPAND     Y2, Y0, Y0
-	VPMOVMSKB Y0, BX
-	ADDQ      $0x40, DI
-	SUBQ      $0x40, CX
-	CMPL      BX, $0xffffffff
+	VPMOVMSKB Y0, DI
+	ADDQ      $0x40, AX
+	SUBQ      $0x40, DX
+	CMPL      DI, $0xffffffff
 	JNE       done
 
 eq32:
-	CMPQ    CX, $0x20
+	CMPQ    DX, $0x20
 	JB      eq16
-	VMOVDQU (AX)(DI*1), Y0
-	VPXOR   (DX)(DI*1), Y0, Y0
-	ADDQ    $0x20, DI
-	SUBQ    $0x20, CX
+	VMOVDQU (CX)(AX*1), Y0
+	VPXOR   (BX)(AX*1), Y0, Y0
+	ADDQ    $0x20, AX
+	SUBQ    $0x20, DX
 	VPTEST  Y8, Y0
 	JNE     done
 
 eq16:
-	CMPQ    CX, $0x10
+	CMPQ    DX, $0x10
 	JB      eq8
-	VMOVDQU (AX)(DI*1), X0
-	VPXOR   (DX)(DI*1), X0, X0
-	ADDQ    $0x10, DI
-	SUBQ    $0x10, CX
+	VMOVDQU (CX)(AX*1), X0
+	VPXOR   (BX)(AX*1), X0, X0
+	ADDQ    $0x10, AX
+	SUBQ    $0x10, DX
 	VPTEST  X8, X0
 	JNE     done
 
 eq8:
-	CMPQ  CX, $0x08
+	CMPQ  DX, $0x08
 	JB    eq4
-	MOVQ  (AX)(DI*1), BX
-	XORQ  (DX)(DI*1), BX
-	ADDQ  $0x08, DI
-	SUBQ  $0x08, CX
-	TESTQ SI, BX
+	MOVQ  (CX)(AX*1), DI
+	XORQ  (BX)(AX*1), DI
+	ADDQ  $0x08, AX
+	SUBQ  $0x08, DX
+	TESTQ SI, DI
 	JNE   done
 	JMP   eq8
 
 eq4:
-	CMPQ  CX, $0x04
+	CMPQ  DX, $0x04
 	JB    eq3
-	MOVL  (AX)(DI*1), BX
-	XORL  (DX)(DI*1), BX
-	ADDQ  $0x04, DI
-	SUBQ  $0x04, CX
-	TESTL $0xdfdfdfdf, BX
+	MOVL  (CX)(AX*1), SI
+	XORL  (BX)(AX*1), SI
+	ADDQ  $0x04, AX
+	SUBQ  $0x04, DX
+	TESTL $0xdfdfdfdf, SI
 	JNE   done
 
 eq3:
-	CMPQ    CX, $0x03
+	CMPQ    DX, $0x03
 	JB      eq2
-	MOVWLZX (AX)(DI*1), BX
-	MOVBLZX 2(AX)(DI*1), AX
-	SHLL    $0x10, AX
-	ORL     BX, AX
-	MOVWLZX (DX)(DI*1), BX
-	MOVBLZX 2(DX)(DI*1), CX
+	MOVWLZX (CX)(AX*1), DX
+	MOVBLZX 2(CX)(AX*1), CX
 	SHLL    $0x10, CX
-	ORL     BX, CX
-	XORL    AX, CX
-	TESTL   $0x00dfdfdf, CX
+	ORL     DX, CX
+	MOVWLZX (BX)(AX*1), DX
+	MOVBLZX 2(BX)(AX*1), AX
+	SHLL    $0x10, AX
+	ORL     DX, AX
+	XORL    CX, AX
+	TESTL   $0x00dfdfdf, AX
 	JMP     done
 
 eq2:
-	CMPQ  CX, $0x02
+	CMPQ  DX, $0x02
 	JB    eq1
-	MOVW  (AX)(DI*1), BX
-	XORW  (DX)(DI*1), BX
-	TESTW $0xdfdf, BX
+	MOVW  (CX)(AX*1), CX
+	XORW  (BX)(AX*1), CX
+	TESTW $0xdfdf, CX
 	JMP   done
 
 eq1:
-	CMPQ  CX, $0x00
+	CMPQ  DX, $0x00
 	JE    done
-	MOVB  (AX)(DI*1), BL
-	XORB  (DX)(DI*1), BL
-	TESTB $0xdf, BL
+	MOVB  (CX)(AX*1), CL
+	XORB  (BX)(AX*1), CL
+	TESTB $0xdf, CL
 
 done:
 	SETEQ ret+32(FP)
 	RET
 
 eq128:
-	VPAND     (AX)(DI*1), Y8, Y0
-	VPAND     (DX)(DI*1), Y8, Y1
+	VPAND     (CX)(AX*1), Y8, Y0
+	VPAND     (BX)(AX*1), Y8, Y1
 	VPCMPEQB  Y1, Y0, Y0
-	VPAND     32(AX)(DI*1), Y8, Y2
-	VPAND     32(DX)(DI*1), Y8, Y3
+	VPAND     32(CX)(AX*1), Y8, Y2
+	VPAND     32(BX)(AX*1), Y8, Y3
 	VPCMPEQB  Y3, Y2, Y2
-	VPAND     64(AX)(DI*1), Y8, Y4
-	VPAND     64(DX)(DI*1), Y8, Y5
+	VPAND     64(CX)(AX*1), Y8, Y4
+	VPAND     64(BX)(AX*1), Y8, Y5
 	VPCMPEQB  Y5, Y4, Y4
-	VPAND     96(AX)(DI*1), Y8, Y6
-	VPAND     96(DX)(DI*1), Y8, Y7
+	VPAND     96(CX)(AX*1), Y8, Y6
+	VPAND     96(BX)(AX*1), Y8, Y7
 	VPCMPEQB  Y7, Y6, Y6
 	VPAND     Y2, Y0, Y0
 	VPAND     Y6, Y4, Y4
 	VPAND     Y4, Y0, Y0
-	VPMOVMSKB Y0, BX
-	ADDQ      $0x80, DI
-	SUBQ      $0x80, CX
-	CMPL      BX, $0xffffffff
+	VPMOVMSKB Y0, DI
+	ADDQ      $0x80, AX
+	SUBQ      $0x80, DX
+	CMPL      DI, $0xffffffff
 	JNE       done
-	CMPQ      CX, $0x80
+	CMPQ      DX, $0x80
 	JB        eq64
 	JMP       eq128
--- build/ascii/equal_fold_asm.go
+++ build/ascii/equal_fold_asm.go
@@ -21,139 +21,174 @@ func main() {
 		"characters only, it allows for greater optimizations.",
 	)
 
-	a := Load(Param("a").Base(), GP64())
-	n := Load(Param("a").Len(), GP64())
-	b := Load(Param("b").Base(), GP64())
-	l, err := Param("b").Len().Resolve()
-	if err != nil {
-		panic(err)
-	}
-
-	ret, err := ReturnIndex(0).Resolve()
-	if err != nil {
-		panic(err)
-	}
-
-	CMPQ(n, l.Addr)       // if a_len != b_len:
-	JNE(LabelRef("done")) //   return false
-
-	eqy := YMM()
-	eqx := XMM()
-	eq := GP64()
-	eqa := GP32()
-	eqb := GP32()
-
-	mask64 := GP64()
-	mask256 := YMM()
-
 	// Use index for byte position. We have plenty of registers, and it saves an
 	// ADD operation as the memory index is the same for both a and b.
 	i := GP64()
-	p := Mem{Base: a, Index: i, Scale: 1}
-	q := Mem{Base: b, Index: i, Scale: 1}
+	a := Mem{Base: Load(Param("a").Base(), GP64()), Index: i, Scale: 1}
+	n := Load(Param("a").Len(), GP64())
+	b := Mem{Base: Load(Param("b").Base(), GP64()), Index: i, Scale: 1}
+	bn, _ := Param("b").Len().Resolve()
+	ret, _ := ReturnIndex(0).Resolve()
 
-	XORQ(i, i)
-	MOVQ(U64(0xDFDFDFDFDFDFDFDF), mask64)
+	CMPQ(n, bn.Addr)      // if len(a) != len(b):
+	JNE(LabelRef("done")) //   return false
+
+	maskG := GP64()
+	maskY := YMM()
+	maskX := maskY.AsX() // use the lower half of maskY
+
+	XORQ(i, i)                           // i = 0
+	MOVQ(U64(0xDFDFDFDFDFDFDFDF), maskG) // maskG = 0xDFDFDFDFDFDFDFDF
 
 	JumpUnlessFeature("eq8", cpu.AVX2)
 
-	PINSRQ(Imm(0), mask64, mask256.AsX())
-	VPBROADCASTQ(mask256.AsX(), mask256)
+	PINSRQ(Imm(0), maskG, maskX) // maskX[0:8] = maskG
+	VPBROADCASTQ(maskX, maskY)   // maskY[0:32] = [maskX[0:8],maskX[0:8],maskX[0:8],maskX[0:8]]
 
 	// Moving the 128-byte scanning helps the branch predictor for small inputs
 	CMPQ(n, U8(128))       // if n >= 128:
 	JNB(LabelRef("eq128")) //   goto eq128
 
 	Label("eq64")
-	CMPQ(n, U8(64))                // if n < 64:
-	JB(LabelRef("eq32"))           //   goto eq32
-	SIMDEQ(p, q, n, i, mask256, 2) // [compare 64 bytes]
+	CMPQ(n, U8(64))       // if n < 64:
+	JB(LabelRef("eq32"))  //   goto eq32
+	EQ64(a, b, n, maskY)  // ZF = [compare 64 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 
 	Label("eq32")
 	CMPQ(n, U8(32))       // if n < 32:
 	JB(LabelRef("eq16"))  //   goto eq16
-	VMOVDQU(p, eqy)       // eqy = a[i:i+32]
-	VPXOR(q, eqy, eqy)    // eqy = b[i:i+32] ^ eqy
-	ADDQ(U8(32), i)       // i += 32
-	SUBQ(U8(32), n)       // n -= 32
-	VPTEST(mask256, eqy)  // if !(mask256 & eqy):
-	JNE(LabelRef("done")) //   return false
+	EQ32(a, b, n, maskY)  // ZF = [compare 32 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 
 	Label("eq16")
-	CMPQ(n, U8(16))            // if n < 16:
-	JB(LabelRef("eq8"))        //   goto eq8
-	VMOVDQU(p, eqx)            // eqx = a[i:i+16]
-	VPXOR(q, eqx, eqx)         // eqx = b[i:i+16] ^ eqx
-	ADDQ(U8(16), i)            // i += 16
-	SUBQ(U8(16), n)            // n -= 16
-	VPTEST(mask256.AsX(), eqx) // if !(mask128 & eqx):
-	JNE(LabelRef("done"))      //   return false
+	CMPQ(n, U8(16))       // if n < 16:
+	JB(LabelRef("eq8"))   //   goto eq8
+	EQ16(a, b, n, maskX)  // ZF = [compare 16 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 
 	Label("eq8")
 	CMPQ(n, U8(8))        // if n < 8:
 	JB(LabelRef("eq4"))   //   goto eq4
-	MOVQ(p, eq)           // eq = a[i:i+8]
-	XORQ(q, eq)           // eq = b[i:i+8] ^ eq
-	ADDQ(U8(8), i)        // i += 8
-	SUBQ(U8(8), n)        // n -= 8
-	TESTQ(mask64, eq)     // if !(mask64 & eq):
-	JNE(LabelRef("done")) //   return false
+	EQ8(a, b, n, maskG)   // ZF = [compare 8 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 	JMP(LabelRef("eq8"))  // loop eq8
 
 	Label("eq4")
-	CMPQ(n, U8(4))                    // if n < 4:
-	JB(LabelRef("eq3"))               //   goto eq3
-	MOVL(p, eq.As32())                // eq = a[i:i+4]
-	XORL(q, eq.As32())                // eq = b[i:i+4] ^ eq
-	ADDQ(U8(4), i)                    // i += 4
-	SUBQ(U8(4), n)                    // n -= 4
-	TESTL(U32(0xDFDFDFDF), eq.As32()) // if !(mask & eq):
-	JNE(LabelRef("done"))             //   return false
+	CMPQ(n, U8(4))        // if n < 4:
+	JB(LabelRef("eq3"))   //   goto eq3
+	EQ4(a, b, n)          // ZF = [compare 4 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 
 	Label("eq3")
-	CMPQ(n, U8(3))            // if n < 3:
-	JB(LabelRef("eq2"))       //   goto eq2
-	MOVWLZX(p, eq.As32())     // eq = a[i:i+2]
-	MOVBLZX(p.Offset(2), eqa) // eqa = a[i+2:i+3]
-	SHLL(U8(16), eqa)         // eqa <<= 16
-	ORL(eq.As32(), eqa)       // eqa = eq | eqa
-	MOVWLZX(q, eq.As32())     // eq = b[i:i+2]
-	MOVBLZX(q.Offset(2), eqb) // eqb = b[i+2:i+3]
-	SHLL(U8(16), eqb)         // eqb <<= 16
-	ORL(eq.As32(), eqb)       // eqb = eq | eqb
-	XORL(eqa, eqb)            // eqb = eqa ^ eqb
-	TESTL(U32(0xDFDFDF), eqb) // return (mask & eqb)
-	JMP(LabelRef("done"))     // ...
+	CMPQ(n, U8(3))        // if n < 3:
+	JB(LabelRef("eq2"))   //   goto eq2
+	EQ3(a, b)             // ZF = [compare 3 bytes]
+	JMP(LabelRef("done")) // return ZF
 
 	Label("eq2")
-	CMPQ(n, U8(2))                // if n < 2:
-	JB(LabelRef("eq1"))           //   goto eq1
-	MOVW(p, eq.As16())            // eq = a[i:i+2]
-	XORW(q, eq.As16())            // eq = b[i:i+2] ^ eq
-	TESTW(U16(0xDFDF), eq.As16()) // return (mask & eq)
-	JMP(LabelRef("done"))         // ...
+	CMPQ(n, U8(2))        // if n < 2:
+	JB(LabelRef("eq1"))   //   goto eq1
+	EQ2(a, b)             // ZF = [compare 2 bytes]
+	JMP(LabelRef("done")) // return ZF
 
 	Label("eq1")
-	CMPQ(n, U8(0))            // if n == 0:
-	JE(LabelRef("done"))      //   return true
-	MOVB(p, eq.As8())         // eq = a[i:i+1]
-	XORB(q, eq.As8())         // eq = b[i:i+1] ^ eq
-	TESTB(U8(0xDF), eq.As8()) // return (mask & eq)
+	CMPQ(n, U8(0))       // if n == 0:
+	JE(LabelRef("done")) //   return true
+	EQ1(a, b)            // ZF = [compare 1 byte]
 
 	Label("done")
-	SETEQ(ret.Addr)
-	RET()
+	SETEQ(ret.Addr) // return ZF
+	RET()           // ...
 
 	Label("eq128")
-	SIMDEQ(p, q, n, i, mask256, 4) // [compare 128 bytes]
-	CMPQ(n, U8(128))               // if n < 128:
-	JB(LabelRef("eq64"))           //   goto eq64
-	JMP(LabelRef("eq128"))         // loop eq128
+	EQ128(a, b, n, maskY)  // ZF = [compare 128 bytes]
+	JNE(LabelRef("done"))  // return if ZF == 0
+	CMPQ(n, U8(128))       // if n < 128:
+	JB(LabelRef("eq64"))   //   goto eq64
+	JMP(LabelRef("eq128")) // loop eq128
 
 	Generate()
 }
 
-func SIMDEQ(p, q Mem, n, i Register, mask256 VecVirtual, lanes int) {
+func EQ1(a, b Mem) {
+	eq := GP8()
+
+	MOVB(a, eq)         // eq = a[i:i+1]
+	XORB(b, eq)         // eq = b[i:i+1] ^ eq
+	TESTB(U8(0xDF), eq) // ZF = (mask & eq)
+}
+
+func EQ2(a, b Mem) {
+	eq := GP16()
+
+	MOVW(a, eq)            // eq = a[i:i+2]
+	XORW(b, eq)            // eq = b[i:i+2] ^ eq
+	TESTW(U16(0xDFDF), eq) // ZF = (mask & eq)
+}
+
+func EQ3(a, b Mem) {
+	eq := GP32()
+	eqa := GP32()
+	eqb := GP32()
+
+	MOVWLZX(a, eq)            // eq = a[i:i+2]
+	MOVBLZX(a.Offset(2), eqa) // eqa = a[i+2:i+3]
+	SHLL(U8(16), eqa)         // eqa <<= 16
+	ORL(eq, eqa)              // eqa = eq | eqa
+	MOVWLZX(b, eq)            // eq = b[i:i+2]
+	MOVBLZX(b.Offset(2), eqb) // eqb = b[i+2:i+3]
+	SHLL(U8(16), eqb)         // eqb <<= 16
+	ORL(eq, eqb)              // eqb = eq | eqb
+	XORL(eqa, eqb)            // eqb = eqa ^ eqb
+	TESTL(U32(0xDFDFDF), eqb) // ZF = (mask & eqb)
+}
+
+func EQ4(a, b Mem, n Register) {
+	eq := GP32()
+
+	MOVL(a, eq)                // eq = a[i:i+4]
+	XORL(b, eq)                // eq = b[i:i+4] ^ eq
+	ADDQ(U8(4), a.Index)       // i += 4
+	SUBQ(U8(4), n)             // n -= 4
+	TESTL(U32(0xDFDFDFDF), eq) // ZF = (mask & eq)
+}
+
+func EQ8(a, b Mem, n, mask Register) {
+	eq := GP64()
+
+	MOVQ(a, eq)          // eq = a[i:i+8]
+	XORQ(b, eq)          // eq = b[i:i+8] ^ eq
+	ADDQ(U8(8), a.Index) // i += 8
+	SUBQ(U8(8), n)       // n -= 8
+	TESTQ(mask, eq)      // ZF = (mask & eq)
+}
+
+func EQ16(a, b Mem, n, mask Register) {
+	EQAVX(a, b, n, XMM(), mask, 16)
+}
+
+func EQ32(a, b Mem, n, mask Register) {
+	EQAVX(a, b, n, YMM(), mask, 32)
+}
+
+func EQ64(a, b Mem, n, mask Register) {
+	EQAVXSIMD(a, b, n, mask, 2)
+}
+
+func EQ128(a, b Mem, n, mask Register) {
+	EQAVXSIMD(a, b, n, mask, 4)
+}
+
+func EQAVX(a, b Mem, n, eq, mask Register, size uint8) {
+	VMOVDQU(a, eq)          // eq = a[i:i+32]
+	VPXOR(b, eq, eq)        // eq = b[i:i+32] ^ eq
+	ADDQ(U8(size), a.Index) // i += size
+	SUBQ(U8(size), n)       // n -= size
+	VPTEST(mask, eq)        // ZF = (mask & eq)
+}
+
+func EQAVXSIMD(a, b Mem, n, mask Register, lanes int) {
 	eq := GP32()
 	and := make([]VecPhysical, 0)
 	ymm := []VecPhysical{Y0, Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9, Y10, Y11, Y12, Y13, Y14, Y15}
@@ -163,9 +198,9 @@ func SIMDEQ(p, q Mem, n, i Register, mask256 VecVirtual, lanes int) {
 		y1 := ymm[2*i+1]
 		and = append(and, y0)
 
-		VPAND(p.Offset(32*i), mask256, y0) // y0 = a[i:i+32] & mask256
-		VPAND(q.Offset(32*i), mask256, y1) // y1 = b[i:i+32] & mask256
-		VPCMPEQB(y1, y0, y0)               // y0 = y1 == y0
+		VPAND(a.Offset(32*i), mask, y0) // y0 = a[i:i+32] & mask
+		VPAND(b.Offset(32*i), mask, y1) // y1 = b[i:i+32] & mask
+		VPCMPEQB(y1, y0, y0)            // y0 = y1 == y0
 	}
 
 	for len(and) > 1 {
@@ -176,9 +211,8 @@ func SIMDEQ(p, q Mem, n, i Register, mask256 VecVirtual, lanes int) {
 		VPAND(y1, y0, y0)
 	}
 
-	VPMOVMSKB(and[0], eq)          // eq[0,1,2,...] = y0[0,8,16,...]
-	ADDQ(Imm(32*uint64(lanes)), i) // i += 32*lanes
-	SUBQ(Imm(32*uint64(lanes)), n) // n -= 32*lanes
-	CMPL(eq, U32(0xFFFFFFFF))      // if eq != 0xFFFFFFFF:
-	JNE(LabelRef("done"))          //   return false
+	VPMOVMSKB(and[0], eq)                // eq[0,1,2,...] = y0[0,8,16,...]
+	ADDQ(Imm(32*uint64(lanes)), a.Index) // i += 32*lanes
+	SUBQ(Imm(32*uint64(lanes)), n)       // n -= 32*lanes
+	CMPL(eq, U32(0xFFFFFFFF))            // ZF = (eq == 0xFFFFFFFF)
 }
