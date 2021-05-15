64c40061de79ce41c7e245b2960ca25921882567
Chris O'Hara <cohara87@gmail.com>
2021-05-15 11:03:59 +1000

    Benchmarks and randomized testing in prep for specializations

--- sortedset/union_test.go
+++ sortedset/union_test.go
@@ -1,9 +1,14 @@
 package sortedset
 
 import (
-	"reflect"
+	"fmt"
+	"math/rand"
 	"testing"
 )
+
+var unionSpecializationSizes = []int{16}
+
+var overlapChances = []float64{0, 0.1, 0.5, 1.0}
 
 func TestUnion(t *testing.T) {
 	for _, test := range []struct {
@@ -60,7 +65,7 @@ func TestUnion(t *testing.T) {
 			expect: []byte{1, 2, 3, 4, 5},
 		},
 		{
-			name:   "size 1, b < a",
+			name:   "size 1, b <= a",
 			a:      []byte{3, 4, 5},
 			b:      []byte{1, 2, 3},
 			size:   1,
@@ -82,13 +87,53 @@ func TestUnion(t *testing.T) {
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
-			dst := make([]byte, 0, len(test.a)+len(test.b))
+			buf := make([]byte, len(test.a)+len(test.b))
+			actual := Union(buf, test.a, test.b, test.size)
+			assertArraysEqual(t, test.expect, actual, test.size)
+		})
+	}
 
-			actual := Union(dst, test.a, test.b, test.size)
-			if (len(test.expect) == 0 && len(actual) != 0) ||
-				(len(test.expect) > 0 && !reflect.DeepEqual(actual, test.expect)) {
-				t.Fatalf("not equal: %v vs expected %v", actual, test.expect)
+	// Test the specializations.
+	for _, size := range unionSpecializationSizes {
+		t.Run(fmt.Sprintf("size %d, random", size), func(t *testing.T) {
+			const maxCount = 100
+			const iterations = 1000
+
+			prng := rand.New(rand.NewSource(0))
+
+			buf := make([]byte, size*maxCount*2)
+
+			for i := 0; i < iterations; i++ {
+				count := prng.Intn(maxCount)
+				for _, p := range overlapChances {
+					setA, setB := randomSortedSetPair(prng, size, count, p)
+					actual := Union(buf[:0], setA, setB, size)
+					expected := Dedupe(combineArrays(setA, setB, size), size)
+					assertArraysEqual(t, expected, actual, size)
+				}
 			}
 		})
+	}
+}
+
+func BenchmarkUnion(b *testing.B) {
+	for _, size := range unionSpecializationSizes {
+		for _, p := range overlapChances {
+			b.Run(fmt.Sprintf("size %d, with %d%% chance of overlap", size, int(p*100)), func(b *testing.B) {
+				const bytes = 64 * 1024
+				prng := rand.New(rand.NewSource(0))
+
+				setA, setB := randomSortedSetPair(prng, size, bytes/size, p)
+
+				buf := make([]byte, bytes*2)
+
+				b.SetBytes(int64(bytes * 2))
+				b.ResetTimer()
+
+				for i := 0; i < b.N; i++ {
+					Union(buf[:0], setA, setB, size)
+				}
+			})
+		}
 	}
 }
