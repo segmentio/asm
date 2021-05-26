364fc24cf63c2e8819605e2530b4b8fec13db705
Jeremy Larkin <jeremy.larkin@segment.com>
2021-05-25 20:30:03 -0700

    fix EqualFoldString

--- ascii/equal_fold_amd64.s
+++ ascii/equal_fold_amd64.s
@@ -12,18 +12,134 @@ TEXT ·EqualFoldString(SB), NOSPLIT, $0-33
 	JNE  done
 	XORQ AX, AX
 	CMPQ DX, $0x10
-	JB   cmp8
+	JB   init_x86
 	BTL  $0x08, github·com∕segmentio∕asm∕cpu·X86+0(SB)
-	JCS  avx
+	JCS  init_avx
+
+init_x86:
+	LEAQ caseMap<>+0(SB), R9
+	XORL SI, SI
 
 cmp8:
-	XORQ AX, AX
+	CMPQ    DX, $0x08
+	JB      cmp7
+	MOVBLZX (CX)(AX*1), DI
+	MOVBLZX (BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 1(CX)(AX*1), DI
+	MOVBLZX 1(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 2(CX)(AX*1), DI
+	MOVBLZX 2(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 3(CX)(AX*1), DI
+	MOVBLZX 3(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 4(CX)(AX*1), DI
+	MOVBLZX 4(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 5(CX)(AX*1), DI
+	MOVBLZX 5(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 6(CX)(AX*1), DI
+	MOVBLZX 6(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	MOVBLZX 7(CX)(AX*1), DI
+	MOVBLZX 7(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+	JNE     done
+	ADDQ    $0x08, AX
+	SUBQ    $0x08, DX
+	JMP     cmp8
+
+cmp7:
+	CMPQ    DX, $0x07
+	JB      cmp6
+	MOVBLZX 6(CX)(AX*1), DI
+	MOVBLZX 6(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+
+cmp6:
+	CMPQ    DX, $0x06
+	JB      cmp5
+	MOVBLZX 5(CX)(AX*1), DI
+	MOVBLZX 5(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+
+cmp5:
+	CMPQ    DX, $0x05
+	JB      cmp4
+	MOVBLZX 4(CX)(AX*1), DI
+	MOVBLZX 4(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+
+cmp4:
+	CMPQ    DX, $0x04
+	JB      cmp3
+	MOVBLZX 3(CX)(AX*1), DI
+	MOVBLZX 3(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+
+cmp3:
+	CMPQ    DX, $0x03
+	JB      cmp2
+	MOVBLZX 2(CX)(AX*1), DI
+	MOVBLZX 2(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+
+cmp2:
+	CMPQ    DX, $0x02
+	JB      cmp1
+	MOVBLZX 1(CX)(AX*1), DI
+	MOVBLZX 1(BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
+
+cmp1:
+	CMPQ    DX, $0x01
+	JB      success
+	MOVBLZX (CX)(AX*1), DI
+	MOVBLZX (BX)(AX*1), R8
+	MOVB    (R9)(DI*1), DI
+	XORB    (R9)(R8*1), DI
+	ORB     DI, SI
 
 done:
 	SETEQ ret+32(FP)
 	RET
 
-avx:
+success:
+	MOVB $0x01, ret+32(FP)
+	RET
+
+init_avx:
 	MOVB         $0x20, SI
 	PINSRB       $0x00, SI, X12
 	VPBROADCASTB X12, Y12
@@ -40,53 +156,53 @@ avx:
 cmp128:
 	CMPQ      DX, $0x80
 	JB        cmp64
-	VLDDQU    (CX)(AX*1), Y0
-	VLDDQU    (BX)(AX*1), Y1
-	VXORPD    Y0, Y1, Y1
-	VPCMPEQB  Y12, Y1, Y2
+	VMOVDQU   (CX)(AX*1), Y0
+	VMOVDQU   32(CX)(AX*1), Y1
+	VMOVDQU   64(CX)(AX*1), Y2
+	VMOVDQU   96(CX)(AX*1), Y3
+	VMOVDQU   (BX)(AX*1), Y4
+	VMOVDQU   32(BX)(AX*1), Y5
+	VMOVDQU   64(BX)(AX*1), Y6
+	VMOVDQU   96(BX)(AX*1), Y7
+	VXORPD    Y0, Y4, Y4
+	VPCMPEQB  Y12, Y4, Y8
 	VORPD     Y12, Y0, Y0
 	VPADDB    Y13, Y0, Y0
 	VPCMPGTB  Y0, Y14, Y0
-	VPAND     Y2, Y0, Y0
+	VPAND     Y8, Y0, Y0
 	VPAND     Y15, Y0, Y0
 	VPSLLW    $0x05, Y0, Y0
-	VPCMPEQB  Y1, Y0, Y0
-	VLDDQU    32(CX)(AX*1), Y3
-	VLDDQU    32(BX)(AX*1), Y4
-	VXORPD    Y3, Y4, Y4
-	VPCMPEQB  Y12, Y4, Y5
+	VPCMPEQB  Y4, Y0, Y0
+	VXORPD    Y1, Y5, Y5
+	VPCMPEQB  Y12, Y5, Y9
+	VORPD     Y12, Y1, Y1
+	VPADDB    Y13, Y1, Y1
+	VPCMPGTB  Y1, Y14, Y1
+	VPAND     Y9, Y1, Y1
+	VPAND     Y15, Y1, Y1
+	VPSLLW    $0x05, Y1, Y1
+	VPCMPEQB  Y5, Y1, Y1
+	VXORPD    Y2, Y6, Y6
+	VPCMPEQB  Y12, Y6, Y10
+	VORPD     Y12, Y2, Y2
+	VPADDB    Y13, Y2, Y2
+	VPCMPGTB  Y2, Y14, Y2
+	VPAND     Y10, Y2, Y2
+	VPAND     Y15, Y2, Y2
+	VPSLLW    $0x05, Y2, Y2
+	VPCMPEQB  Y6, Y2, Y2
+	VXORPD    Y3, Y7, Y7
+	VPCMPEQB  Y12, Y7, Y11
 	VORPD     Y12, Y3, Y3
 	VPADDB    Y13, Y3, Y3
 	VPCMPGTB  Y3, Y14, Y3
-	VPAND     Y5, Y3, Y3
+	VPAND     Y11, Y3, Y3
 	VPAND     Y15, Y3, Y3
 	VPSLLW    $0x05, Y3, Y3
-	VPCMPEQB  Y4, Y3, Y3
-	VLDDQU    64(CX)(AX*1), Y6
-	VLDDQU    64(BX)(AX*1), Y7
-	VXORPD    Y6, Y7, Y7
-	VPCMPEQB  Y12, Y7, Y8
-	VORPD     Y12, Y6, Y6
-	VPADDB    Y13, Y6, Y6
-	VPCMPGTB  Y6, Y14, Y6
-	VPAND     Y8, Y6, Y6
-	VPAND     Y15, Y6, Y6
-	VPSLLW    $0x05, Y6, Y6
-	VPCMPEQB  Y7, Y6, Y6
-	VLDDQU    96(CX)(AX*1), Y9
-	VLDDQU    96(BX)(AX*1), Y10
-	VXORPD    Y9, Y10, Y10
-	VPCMPEQB  Y12, Y10, Y11
-	VORPD     Y12, Y9, Y9
-	VPADDB    Y13, Y9, Y9
-	VPCMPGTB  Y9, Y14, Y9
-	VPAND     Y11, Y9, Y9
-	VPAND     Y15, Y9, Y9
-	VPSLLW    $0x05, Y9, Y9
-	VPCMPEQB  Y10, Y9, Y9
-	VPAND     Y3, Y0, Y0
-	VPAND     Y9, Y6, Y6
-	VPAND     Y6, Y0, Y0
+	VPCMPEQB  Y7, Y3, Y3
+	VPAND     Y1, Y0, Y0
+	VPAND     Y3, Y2, Y2
+	VPAND     Y2, Y0, Y0
 	ADDQ      $0x80, AX
 	SUBQ      $0x80, DX
 	VPMOVMSKB Y0, SI
@@ -97,29 +213,29 @@ cmp128:
 cmp64:
 	CMPQ      DX, $0x40
 	JB        cmp32
-	VLDDQU    (CX)(AX*1), Y0
-	VLDDQU    (BX)(AX*1), Y1
-	VXORPD    Y0, Y1, Y1
-	VPCMPEQB  Y12, Y1, Y2
+	VMOVDQU   (CX)(AX*1), Y0
+	VMOVDQU   32(CX)(AX*1), Y1
+	VMOVDQU   (BX)(AX*1), Y2
+	VMOVDQU   32(BX)(AX*1), Y3
+	VXORPD    Y0, Y2, Y2
+	VPCMPEQB  Y12, Y2, Y4
 	VORPD     Y12, Y0, Y0
 	VPADDB    Y13, Y0, Y0
 	VPCMPGTB  Y0, Y14, Y0
-	VPAND     Y2, Y0, Y0
+	VPAND     Y4, Y0, Y0
 	VPAND     Y15, Y0, Y0
 	VPSLLW    $0x05, Y0, Y0
-	VPCMPEQB  Y1, Y0, Y0
-	VLDDQU    32(CX)(AX*1), Y3
-	VLDDQU    32(BX)(AX*1), Y4
-	VXORPD    Y3, Y4, Y4
-	VPCMPEQB  Y12, Y4, Y5
-	VORPD     Y12, Y3, Y3
-	VPADDB    Y13, Y3, Y3
-	VPCMPGTB  Y3, Y14, Y3
-	VPAND     Y5, Y3, Y3
-	VPAND     Y15, Y3, Y3
-	VPSLLW    $0x05, Y3, Y3
-	VPCMPEQB  Y4, Y3, Y3
-	VPAND     Y3, Y0, Y0
+	VPCMPEQB  Y2, Y0, Y0
+	VXORPD    Y1, Y3, Y3
+	VPCMPEQB  Y12, Y3, Y5
+	VORPD     Y12, Y1, Y1
+	VPADDB    Y13, Y1, Y1
+	VPCMPGTB  Y1, Y14, Y1
+	VPAND     Y5, Y1, Y1
+	VPAND     Y15, Y1, Y1
+	VPSLLW    $0x05, Y1, Y1
+	VPCMPEQB  Y3, Y1, Y1
+	VPAND     Y1, Y0, Y0
 	ADDQ      $0x40, AX
 	SUBQ      $0x40, DX
 	VPMOVMSKB Y0, SI
@@ -129,8 +245,8 @@ cmp64:
 cmp32:
 	CMPQ      DX, $0x20
 	JB        cmp16
-	VLDDQU    (CX)(AX*1), Y0
-	VLDDQU    (BX)(AX*1), Y1
+	VMOVDQU   (CX)(AX*1), Y0
+	VMOVDQU   (BX)(AX*1), Y1
 	VXORPD    Y0, Y1, Y1
 	VPCMPEQB  Y12, Y1, Y2
 	VORPD     Y12, Y0, Y0
@@ -148,9 +264,9 @@ cmp32:
 
 cmp16:
 	CMPQ      DX, $0x10
-	JB        cmp8
-	VLDDQU    (CX)(AX*1), X0
-	VLDDQU    (BX)(AX*1), X1
+	JLE       cmp_tail
+	VMOVDQU   (CX)(AX*1), X0
+	VMOVDQU   (BX)(AX*1), X1
 	VXORPD    X0, X1, X1
 	VPCMPEQB  X12, X1, X2
 	VORPD     X12, X0, X0
@@ -162,9 +278,27 @@ cmp16:
 	VPCMPEQB  X1, X0, X0
 	ADDQ      $0x10, AX
 	SUBQ      $0x10, DX
-	VPMOVMSKB X0, AX
-	XORL      $0x0000ffff, AX
+	VPMOVMSKB X0, SI
+	XORL      $0x0000ffff, SI
 	JNE       done
-	CMPQ      DX, $0x00
-	JE        done
-	JMP       cmp8
+
+cmp_tail:
+	SUBQ      $0x10, DX
+	ADDQ      DX, AX
+	VMOVDQU   (CX)(AX*1), X0
+	VMOVDQU   (BX)(AX*1), X1
+	VXORPD    X0, X1, X1
+	VPCMPEQB  X12, X1, X2
+	VORPD     X12, X0, X0
+	VPADDB    X13, X0, X0
+	VPCMPGTB  X0, X14, X0
+	VPAND     X2, X0, X0
+	VPAND     X15, X0, X0
+	VPSLLW    $0x05, X0, X0
+	VPCMPEQB  X1, X0, X0
+	VPMOVMSKB X0, SI
+	XORL      $0x0000ffff, SI
+	JMP       done
+
+DATA caseMap<>+0(SB)/256, $"\x00\x01\x02\x03\x04\x05\x06\a\b\t\n\v\f\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@abcdefghijklmnopqrstuvwxyz[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u007f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
+GLOBL caseMap<>(SB), RODATA|NOPTR, $256
--- build/ascii/equal_fold_asm.go
+++ build/ascii/equal_fold_asm.go
@@ -8,8 +8,28 @@ import (
 	. "github.com/mmcloughlin/avo/reg"
 	. "github.com/segmentio/asm/build/internal/x86"
 
+	"fmt"
+
 	"github.com/segmentio/asm/cpu"
 )
+
+const caseMapData = String("" +
+	"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
+	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" +
+	"\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f" +
+	"\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f" +
+	"\x40\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f" +
+	"\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x5b\x5c\x5d\x5e\x5f" +
+	"\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f" +
+	"\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f" +
+	"\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f" +
+	"\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f" +
+	"\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf" +
+	"\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf" +
+	"\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf" +
+	"\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf" +
+	"\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef" +
+	"\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff")
 
 func main() {
 	TEXT("EqualFoldString", NOSPLIT, "func(a, b string) bool")
@@ -29,107 +49,142 @@ func main() {
 	b := Mem{Base: Load(Param("b").Base(), GP64()), Index: i, Scale: 1}
 	bn, _ := Param("b").Len().Resolve()
 	ret, _ := ReturnIndex(0).Resolve()
+	tmp := GP32()
 
 	CMPQ(n, bn.Addr)      // if len(a) != len(b):
 	JNE(LabelRef("done")) //   return false
 	XORQ(i, i)            // i = 0
 
-	CMPQ(n, U8(16))                // if n < 16:
-	JB(LabelRef("cmp8"))           //   goto cmp8
-	JumpIfFeature("avx", cpu.AVX2) // goto avx if supported
+	CMPQ(n, U8(16))                     // if n < 16:
+	JB(LabelRef("init_x86"))            //   goto init_x86
+	JumpIfFeature("init_avx", cpu.AVX2) // goto init_avx if supported
+
+	Label("init_x86")
+
+	cmp := GP32()
+	av := GP32()
+	bv := GP32()
+
+	// Map to convert ASCII upper characters to lower case.
+	caseMap := Mem{Base: GP64(), Scale: 1}
+	LEAQ(ConstData("caseMap", caseMapData), caseMap.Base)
+	XORL(cmp, cmp)
 
 	Label("cmp8")
-	// TODO: add fallback
-	t := GP64()
-	XORQ(t, t)
+	CMPQ(n, U8(8))       // if n < 0:
+	JB(LabelRef("cmp7")) //   goto cmp7
+	for i := 0; i < 8; i++ {
+		MOVBLZX(a.Offset(i), av)           // av = a[i]
+		MOVBLZX(b.Offset(i), bv)           // bv = b[i]
+		MOVB(caseMap.Idx(av, 1), av.As8()) // av = caseMap[av]
+		XORB(caseMap.Idx(bv, 1), av.As8()) // av = caseMap[bv] ^ av
+		ORB(av.As8(), cmp.As8())           // cmp |= av
+	}
+	JNE(LabelRef("done")) // return false if ZF == 0
+	ADDQ(Imm(8), a.Index) // i += 8
+	SUBQ(Imm(8), n)       // n -= 8
+	JMP(LabelRef("cmp8"))
+
+	for i := 6; i >= 0; i-- {
+		Label(fmt.Sprintf("cmp%d", i+1))
+		next := "success"
+		if i > 0 {
+			next = fmt.Sprintf("cmp%d", i)
+		}
+
+		CMPQ(n, U8(i+1))                   // if n < i:
+		JB(LabelRef(next))                 //   goto cmp${i-1}
+		MOVBLZX(a.Offset(i), av)           // av = a[i]
+		MOVBLZX(b.Offset(i), bv)           // bv = b[i]
+		MOVB(caseMap.Idx(av, 1), av.As8()) // av = caseMap[av]
+		XORB(caseMap.Idx(bv, 1), av.As8()) // av = caseMap[bv] ^ av
+		ORB(av.As8(), cmp.As8())           // cmp |= av
+	}
 
 	Label("done")
 	SETEQ(ret.Addr) // return ZF
 	RET()           // ...
 
-	Label("avx")
+	Label("success")
+	MOVB(U8(1), ret.Addr) // return true
+	RET()                 // ...
 
-	maskY := [...]Register{
-		VecBroadcast(U8(0x20), YMM()), // "case" bit
-		VecBroadcast(U8(0x1F), YMM()), // 0b10000000 - 'a'
-		VecBroadcast(U8(0x9A), YMM()), // 'z' - 'a' + 1 - 0x80 (overflowed 8-bits)
-		VecBroadcast(U8(0x01), YMM()), // 1-bit for testing
-	}
+	Label("init_avx")
 
-	maskX := [...]Register{
-		maskY[0].(Vec).AsX(),
-		maskY[1].(Vec).AsX(),
-		maskY[2].(Vec).AsX(),
-		maskY[3].(Vec).AsX(),
+	bit := VecBroadcast(U8(0x20), YMM()) // "case" bit
+	msk := VecBroadcast(U8(0x1F), YMM()) // 0b10000000 - 'a'
+	rng := VecBroadcast(U8(0x9A), YMM()) // 'z' - 'a' + 1 - 0x80 (overflowed 8-bits)
+	one := VecBroadcast(U8(0x01), YMM()) // 1-bit for ANDing with comparison
+
+	vec := NewVectorizer(12, func(l VectorLane) Register {
+		v0 := l.Read(a)
+		v1 := l.Read(b)
+		v2 := l.Alloc()
+
+		VXORPD(v0, v1, v1)     // calculate difference between a and b
+		VPCMPEQB(bit, v1, v2)  // check if above difference is the 6th bit
+		VORPD(bit, v0, v0)     // set the 6th bit for a
+		VPADDB(msk, v0, v0)    // add 0x1f to each byte to set top bit for letters
+		VPCMPGTB(v0, rng, v0)  // compare if not letter: v - 'a' < 'z' - 'a' + 1
+		VPAND(v2, v0, v0)      // combine 6th-bit difference with letter range
+		VPAND(one, v0, v0)     // merge test mask
+		VPSLLW(Imm(5), v0, v0) // shift into case bit position
+		VPCMPEQB(v1, v0, v0)   // compare original difference with case-only difference
+
+		return v0
+	}).Reduce(ReduceAnd) // merge all comparisons together
+
+	cmpAVX := func(spec Spec, lanes int, incr bool) {
+		sz := int(spec.Size())
+		out := vec.Compile(spec, lanes)[0] // [compare sz*lanes bytes]
+		if incr {
+			ADDQ(U8(sz*lanes), a.Index) // i += sz*lanes
+			SUBQ(U8(sz*lanes), n)       // n -= sz*lanes
+		}
+		VPMOVMSKB(out, tmp)                 // tmp[0,1,2,...] = y0[0,8,16,...]
+		XORL(U32(^uint32(0)>>(32-sz)), tmp) // ZF = (tmp == 0xFFFFFFFF)
 	}
 
 	Label("cmp128")
-	CMPQ(n, U8(128))                  // if n < 128:
-	JB(LabelRef("cmp64"))             //   goto cmp64
-	equalAVX(a, b, n, maskY, 4, S256) // ZF = [compare 128 bytes]
-	JNE(LabelRef("done"))             // return if ZF == 0
-	JMP(LabelRef("cmp128"))           // loop cmp128
+	CMPQ(n, U8(128))        // if n < 128:
+	JB(LabelRef("cmp64"))   //   goto cmp64
+	cmpAVX(S256, 4, true)   // ZF = [compare 128 bytes]
+	JNE(LabelRef("done"))   // return if ZF == 0
+	JMP(LabelRef("cmp128")) // loop cmp128
 
 	Label("cmp64")
-	CMPQ(n, U8(64))                   // if n < 64:
-	JB(LabelRef("cmp32"))             //   goto cmp32
-	equalAVX(a, b, n, maskY, 2, S256) // ZF = [compare 64 bytes]
-	JNE(LabelRef("done"))             // return ZF if ZF == 0
+	CMPQ(n, U8(64))       // if n < 64:
+	JB(LabelRef("cmp32")) //   goto cmp32
+	cmpAVX(S256, 2, true) // ZF = [compare 64 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 
 	Label("cmp32")
-	CMPQ(n, U8(32))                   // if n < 32:
-	JB(LabelRef("cmp16"))             //   goto cmp16
-	equalAVX(a, b, n, maskY, 1, S256) // ZF = [compare 32 bytes]
-	JNE(LabelRef("done"))             // return ZF if ZF == 0
+	CMPQ(n, U8(32))       // if n < 32:
+	JB(LabelRef("cmp16")) //   goto cmp16
+	cmpAVX(S256, 1, true) // ZF = [compare 32 bytes]
+	JNE(LabelRef("done")) // return if ZF == 0
 
 	Label("cmp16")
-	CMPQ(n, U8(16))                   // if n < 16:
-	JB(LabelRef("cmp8"))              //   goto cmp8
-	equalAVX(a, b, n, maskX, 1, S128) // ZF = [compare 16 bytes]
-	JNE(LabelRef("done"))             // return ZF if ZF == 0
 
-	CMPQ(n, U8(0))        // if n == 0:
-	JE(LabelRef("done"))  //   goto done
-	JMP(LabelRef("cmp8")) // gpto cmp8
+	// Convert YMM masks to XMM
+	bit = bit.(Vec).AsX()
+	msk = msk.(Vec).AsX()
+	rng = rng.(Vec).AsX()
+	one = one.(Vec).AsX()
+
+	CMPQ(n, U8(16))           // if n <= 16:
+	JLE(LabelRef("cmp_tail")) //   goto cmp_tail
+	cmpAVX(S128, 1, true)     // ZF = [compare 16 bytes]
+	JNE(LabelRef("done"))     // return if ZF == 0
+
+	Label("cmp_tail")
+	// At this point, we have <= 16 bytes to compare, but we know the total input
+	// is >= 16 bytes. Move the pointer to the *last* 16 bytes of the input so we
+	// can skip the fallback.
+	SUBQ(Imm(16), n)       // n -= 16
+	ADDQ(n, a.Index)       // i += n
+	cmpAVX(S128, 1, false) // ZF = [compare 16 bytes]
+	JMP(LabelRef("done"))  // return ZF
 
 	Generate()
-}
-
-func equalAVX(a, b Mem, n Register, mask [4]Register, lanes int, s Spec) {
-	msk := GP32()
-	out := make([]VecPhysical, 0)
-	vec := VecList(s, 12)
-	sz := int(s.Size())
-
-	for i := 0; i < lanes; i++ {
-		v0 := vec[3*i]
-		v1 := vec[3*i+1]
-		v2 := vec[3*i+2]
-		out = append(out, v0)
-
-		VLDDQU(a.Offset(sz*i), v0) // load 32 bytes from a
-		VLDDQU(b.Offset(sz*i), v1) // load 32 bytes from b
-		VXORPD(v0, v1, v1)         // calculate difference between a and b
-		VPCMPEQB(mask[0], v1, v2)  // check if above difference is the 6th bit
-		VORPD(mask[0], v0, v0)     // set the 6th bit for a
-		VPADDB(mask[1], v0, v0)    // add 0x1f to each byte to set top bit for letters
-		VPCMPGTB(v0, mask[2], v0)  // compare if not letter: v - 'a' < 'z' - 'a' + 1
-		VPAND(v2, v0, v0)          // combine 6th-bit difference with letter range
-		VPAND(mask[3], v0, v0)     // merge test mask
-		VPSLLW(Imm(5), v0, v0)     // shift into case bit position
-		VPCMPEQB(v1, v0, v0)       // compare original difference with case-only difference
-	}
-
-	for len(out) > 1 {
-		v0 := out[0]
-		v1 := out[1]
-		out = append(out[2:], v0)
-
-		VPAND(v1, v0, v0) // merge all comparisons together
-	}
-
-	ADDQ(Imm(uint64(sz*lanes)), a.Index) // i += sz*lanes
-	SUBQ(Imm(uint64(sz*lanes)), n)       // n -= sz*lanes
-	VPMOVMSKB(out[0], msk)               // msk[0,1,2,...] = y0[0,8,16,...]
-	XORL(U32(^uint32(0)>>(32-sz)), msk)  // ZF = (msk == 0xFFFFFFFF)
 }
