65736ace629e0b5614bb7a5bf063c267e0dfef35
Achille Roussel <achille@segment.com>
2021-05-28 22:17:57 -0700

    add mem.Mask

--- mem/mask_test.go
+++ mem/mask_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/segmentio/asm/mem"
 )
 
-func TestBlend(t *testing.T) {
+func TestMask(t *testing.T) {
 	for _, N := range []int{0, 1, 2, 3, 4, 8, 10, 31, 32, 100, 1024, 4096} {
 		t.Run(fmt.Sprintf("N=%d", N), func(t *testing.T) {
 			src := make([]byte, N)
@@ -23,16 +23,16 @@ func TestBlend(t *testing.T) {
 
 			copy(exp, dst)
 			for i := range src {
-				exp[i] |= src[i]
+				exp[i] &= src[i]
 			}
 
-			n := mem.Blend(dst, src)
+			n := mem.Mask(dst, src)
 			if n != N {
-				t.Errorf("blending did not apply to enough bytes: %d != %d", n, N)
+				t.Errorf("masking did not apply to enough bytes: %d != %d", n, N)
 			}
 
 			if !bytes.Equal(dst, exp) {
-				t.Error("blending produced the wrong output")
+				t.Error("masking produced the wrong output")
 				t.Logf("expected: %08b", limit(exp, 8))
 				t.Logf("found:    %08b", limit(dst, 8))
 				t.Logf("source:   %08b", limit(src, 8))
@@ -41,7 +41,7 @@ func TestBlend(t *testing.T) {
 	}
 }
 
-func BenchmarkBlend(b *testing.B) {
+func BenchmarkMask(b *testing.B) {
 	for _, N := range []int{7, 10, 31, 32, 100, 1024, 4096} {
 		b.Run(fmt.Sprintf("N=%d", N), func(b *testing.B) {
 			dst := make([]byte, N)
@@ -51,7 +51,7 @@ func BenchmarkBlend(b *testing.B) {
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
-				mem.Blend(dst, src)
+				mem.Mask(dst, src)
 			}
 		})
 	}
