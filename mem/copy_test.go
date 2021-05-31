f38203a5ed7338e5c790019202b8e909d5a26a1f
Achille Roussel <achille@segment.com>
2021-05-30 17:10:11 -0700

    factorize mem tests

--- mem/copy_test.go
+++ mem/copy_test.go
@@ -1,56 +1,11 @@
-package mem_test
+package mem
 
-import (
-	"bytes"
-	"encoding/hex"
-	"fmt"
-	"io"
-	"math/rand"
-	"testing"
-
-	"github.com/segmentio/asm/mem"
-)
+import "testing"
 
 func TestCopy(t *testing.T) {
-	for _, N := range []int{0, 1, 2, 3, 4, 6, 8, 10, 31, 32, 33, 64, 100, 1024, 4096} {
-		t.Run(fmt.Sprintf("N=%d", N), func(t *testing.T) {
-			src := make([]byte, N)
-			dst := make([]byte, N)
-			exp := make([]byte, N)
-
-			prng := rand.New(rand.NewSource(0))
-			io.ReadFull(prng, src)
-			io.ReadFull(prng, dst)
-
-			copy(exp, src)
-
-			n := mem.Copy(dst, src)
-			if n != N {
-				t.Errorf("copying did not apply to enough bytes: %d != %d", n, N)
-			}
-
-			if !bytes.Equal(dst, exp) {
-				t.Error("copying produced the wrong output")
-				t.Logf("expected:\n%s", hex.Dump(exp))
-				t.Logf("found:   \n%s", hex.Dump(dst))
-				t.Logf("source:  \n%s", hex.Dump(src))
-			}
-		})
-	}
+	testCopy(t, Copy, copyGeneric)
 }
 
 func BenchmarkCopy(b *testing.B) {
-	for _, N := range []int{7, 10, 31, 32, 100, 1024, 4096} {
-		b.Run(fmt.Sprintf("N=%d", N), func(b *testing.B) {
-			dst := make([]byte, N)
-			src := make([]byte, N)
-			io.ReadFull(rand.New(rand.NewSource(0)), src)
-			b.SetBytes(int64(N))
-			b.ResetTimer()
-
-			for i := 0; i < b.N; i++ {
-				mem.Copy(dst, src)
-			}
-		})
-	}
+	benchmarkCopy(b, Copy)
 }
--- mem/mask_default.go
+++ mem/mask_default.go
@@ -5,16 +5,5 @@ package mem
 // Mask performs a AND of src and dst into dst, returning the number of bytes
 // written to dst.
 func Mask(dst, src []byte) int {
-	switch {
-	case len(dst) < len(src):
-		src = src[:len(dst)]
-	case len(dst) > len(src):
-		dst = dst[:len(src)]
-	}
-
-	for i := range dst {
-		dst[i] &= src[i]
-	}
-
-	return len(dst)
+	return maskGeneric(dst, src)
 }
