5182baf558530994a8070b48c3fafee33c2278a4
Chris O'Hara <cohara87@gmail.com>
2021-06-04 19:23:13 +1000

    Fix for !amd64

---
+++ qsort/hybrid_default.go
@@ -0,0 +1,15 @@
+// +build !amd64
+
+package qsort
+
+func insertionsort32(data *byte, lo int, hi int) {
+	panic("not implemented")
+}
+
+func distributeForward32(data *byte, scratch *byte, limit int, lo int, hi int, pivot int) int {
+	panic("not implemented")
+}
+
+func distributeBackward32(data *byte, scratch *byte, limit int, lo int, hi int, pivot int) int {
+	panic("not implemented")
+}
