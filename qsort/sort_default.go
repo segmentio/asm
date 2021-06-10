bdadf5dc320f22391dd0a2c75312a80e1adac8e4
Chris O'Hara <cohara87@gmail.com>
2021-06-10 22:00:03 +1000

    Fix tests

--- qsort/sort_default.go
+++ qsort/sort_default.go
@@ -2,11 +2,11 @@
 
 package qsort
 
-func distributeForward64(data, scratch *byte, limit, lo, hi, pivot int) int {
+func distributeForward64(data, scratch *byte, limit, lo, hi int) int {
 	panic("not implemented")
 }
 
-func distributeBackward64(data, scratch *byte, limit, lo, hi, pivot int) int {
+func distributeBackward64(data, scratch *byte, limit, lo, hi int) int {
 	panic("not implemented")
 }
 
@@ -14,11 +14,11 @@ func insertionsort128NoSwapAsm(data []byte) {
 	panic("not implemented")
 }
 
-func distributeForward128(data, scratch *byte, limit, lo, hi, pivot int) int {
+func distributeForward128(data, scratch *byte, limit, lo, hi int) int {
 	panic("not implemented")
 }
 
-func distributeBackward128(data, scratch *byte, limit, lo, hi, pivot int) int {
+func distributeBackward128(data, scratch *byte, limit, lo, hi int) int {
 	panic("not implemented")
 }
 
@@ -26,10 +26,10 @@ func insertionsort256NoSwapAsm(data []byte) {
 	panic("not implemented")
 }
 
-func distributeForward256(data, scratch *byte, limit, lo, hi, pivot int) int {
+func distributeForward256(data, scratch *byte, limit, lo, hi int) int {
 	panic("not implemented")
 }
 
-func distributeBackward256(data, scratch *byte, limit, lo, hi, pivot int) int {
+func distributeBackward256(data, scratch *byte, limit, lo, hi int) int {
 	panic("not implemented")
 }
