5faa4f0ce01909084f5069e3693b952b004346f1
Jeremy Larkin <jeremy.larkin@segment.com>
2021-06-28 10:19:58 -0700

    add newline test and benchmark

--- base64/base64_test.go
+++ base64/base64_test.go
@@ -65,11 +65,11 @@ func TestEncoding(t *testing.T) {
 				nCandidate, errCandidate := enc.candidate.Decode(decActual, encActual)
 
 				if errControl != nil {
-					t.Fatalf("control decode failed: %v", errControl)
+					t.Fatalf("control decode error: %v", errControl)
 				}
 
 				if errCandidate != nil {
-					t.Fatalf("candidate decode failed: %v", errCandidate)
+					t.Fatalf("candidate decode error: %v", errCandidate)
 				}
 
 				if nControl != nCandidate {
@@ -88,6 +88,27 @@ func TestEncoding(t *testing.T) {
 	}
 }
 
+func TestDecodeLines(t *testing.T) {
+
+	src := []byte(`dGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRl
+c3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0
+IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0
+ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVzdCB0ZXN0IHRlc3QgdGVz
+dCB0ZXN0IHRlc3QgdGVzdA==`)
+
+	expect := []byte(`test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test`)
+	actual := make([]byte, StdEncoding.DecodedLen(len(src)))
+	n, err := StdEncoding.Decode(actual, src)
+
+	if err != nil {
+		t.Fatalf("decode error: %v", err)
+	}
+
+	if !bytes.Equal(expect, actual[:n]) {
+		t.Errorf("failed decode:\n\texpect = %v\n\tactual = %v", expect, actual)
+	}
+}
+
 func BenchmarkEncode(b *testing.B) {
 	src := make([]byte, 4096)
 	dst := make([]byte, base64.StdEncoding.EncodedLen(len(src)))
@@ -103,6 +124,29 @@ func BenchmarkEncode(b *testing.B) {
 	b.Run("go", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			base64.StdEncoding.Encode(dst, src)
+		}
+		b.SetBytes(int64(len(src)))
+	})
+}
+
+func BenchmarkDecode(b *testing.B) {
+	raw := make([]byte, 4096)
+	src := make([]byte, base64.StdEncoding.EncodedLen(len(raw)))
+	dst := make([]byte, base64.StdEncoding.DecodedLen(len(src)))
+
+	rand.Read(raw)
+	base64.StdEncoding.Encode(src, raw)
+
+	b.Run("asm", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			StdEncoding.Decode(dst, src)
+		}
+		b.SetBytes(int64(len(src)))
+	})
+
+	b.Run("go", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			base64.StdEncoding.Decode(dst, src)
 		}
 		b.SetBytes(int64(len(src)))
 	})
