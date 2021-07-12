3842482fd0cedf0e89f153647c5b311060282eb4
Jeremy Larkin <jeremy.larkin@segment.com>
2021-07-12 13:55:43 -0700

    swap to use protected buffers

--- base64/base64_amd64_test.go
+++ base64/base64_amd64_test.go
@@ -6,51 +6,101 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/segmentio/asm/internal/buffer"
 )
 
+func fillBuffers(b *buffer.Buffer, size int) map[string][]byte {
+	bufs := map[string][]byte{
+		"head": b.ProtectHead(),
+		"tail": b.ProtectTail(),
+	}
+
+	for _, buf := range bufs {
+		if true {
+			rand.Seed(0)
+			rand.Read(buf)
+		} else {
+			for i := 0; i < size; i++ {
+				buf[i] = (255 - byte(i&15)*16) - byte(i&255)/16
+			}
+		}
+	}
+
+	return bufs
+}
+
 func TestEncodeAVX2(t *testing.T) {
+	b, err := buffer.New(512)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer b.Release()
+
+	bufs := fillBuffers(&b, 512)
+
 	for _, enc := range encodings {
-		for i := minEncodeLen; i < 1024; i++ {
-			ok := t.Run(fmt.Sprintf("%s-%d", enc.name, i), func(t *testing.T) {
-				src := make([]byte, i)
-				dst := make([]byte, enc.candidate.EncodedLen(i))
+		if enc.candidate.enc == nil {
+			t.Log("AVX2 not enabled")
+			continue
+		}
+		for name, buf := range bufs {
+			ok := t.Run(fmt.Sprintf("%s-%s", enc.name, name), func(t *testing.T) {
+				dst, err := buffer.New(enc.candidate.EncodedLen(len(buf)))
+				if err != nil {
+					t.Fatal(err)
+				}
+				defer dst.Release()
 
-				rand.Read(src)
+				_, ns := enc.candidate.enc(dst.ProtectTail(), buf, enc.candidate.enclut)
 
-				_, ns := encodeAVX2(dst, src, enc.candidate.enc)
-
-				if i-ns >= 32 {
-					t.Errorf("encodeAVX2 remain should be less than 32, but is %d", i-ns)
+				if len(buf)-ns >= 32 {
+					t.Errorf("encode remain should be less than 32, but is %d", len(buf)-ns)
 				}
 			})
 
 			if !ok {
-				return
+				break
 			}
 		}
 	}
 }
 
 func TestDecodeAVX2(t *testing.T) {
+	b, err := buffer.New(512)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer b.Release()
+
+	bufs := fillBuffers(&b, 512)
+
 	for _, enc := range encodings {
-		for i := 34; i < 1024; i++ {
-			ok := t.Run(fmt.Sprintf("%s-%d", enc.name, i), func(t *testing.T) {
-				raw := make([]byte, i)
-				src := make([]byte, enc.candidate.EncodedLen(i))
-				dst := make([]byte, i)
+		if enc.candidate.dec == nil {
+			t.Log("AVX2 not enabled")
+			continue
+		}
 
-				rand.Read(raw)
-				enc.candidate.Encode(src, raw)
+		for name, buf := range bufs {
+			ok := t.Run(fmt.Sprintf("%s-%s", enc.name, name), func(t *testing.T) {
+				src := make([]byte, enc.candidate.EncodedLen(len(buf)))
+				dst, err := buffer.New(len(buf))
+				if err != nil {
+					t.Fatal(err)
+				}
+				defer dst.Release()
 
-				_, ns := decodeAVX2(dst, src, enc.candidate.dec)
+				enc.candidate.Encode(src, buf)
 
-				if i-ns >= 45 {
-					t.Errorf("decodeAVX2 remain should be less than 45, but is %d", i-ns)
+				_, ns := enc.candidate.dec(dst.ProtectTail(), src, enc.candidate.declut)
+
+				if len(buf)-ns >= 45 {
+					t.Errorf("decode remain should be less than 45, but is %d", len(buf)-ns)
 				}
 			})
 
 			if !ok {
-				return
+				break
 			}
 		}
 	}
