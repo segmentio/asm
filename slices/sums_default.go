e5e00bc40fe1d792ce26b22de04238c5c269bd87
Ray Jenkins <ray.jenkins@gmail.com>
2021-07-15 07:33:48 -0500

    correct sums_default types for sumUint8

--- slices/sums_default.go
+++ slices/sums_default.go
@@ -2,18 +2,18 @@
 
 package slices
 
-func sumUint64(x []uint64, y []uint64) {
+func sumUint64(x, y []uint64) {
 	sumUint64Generic(x, y)
 }
 
-func sumUint32(x []uint32, y []uint32) {
+func sumUint32(x, y []uint32) {
 	sumUint32Generic(x, y)
 }
 
-func sumUint16(x []uint16, y []uint16) {
+func sumUint16(x, y []uint16) {
 	sumUint16Generic(x, y)
 }
 
-func sumUint8(x []uint16, y []uint16) {
+func sumUint8(x, y []uint8) {
 	sumUint8Generic(x, y)
 }
