41fa61599369e86ef8ad7adf0fa951dc924c322e
Thomas Pelletier <thomas.pelletier@segment.com>
2021-11-23 14:13:25 -0500

    Default to stdlib for non-amd64 platforms

---
+++ utf8/valid_default.go
@@ -0,0 +1,10 @@
+//go:build purego || !amd64
+
+package utf8
+
+import stdlib "unicode/utf8"
+
+// Valid reports whether p consists entirely of valid UTF-8-encoded runes.
+func Valid(p []byte) bool {
+	return stdlib.Valid(p)
+}
