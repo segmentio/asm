e9607687afa1549916442c98d3024668a78e1966
Thomas Pelletier <thomas.pelletier@segment.com>
2022-01-04 09:55:04 -0500

    Check for ASCII validation in fuzzer

--- utf8/valid_fuzz_test.go
+++ utf8/valid_fuzz_test.go
@@ -6,14 +6,20 @@ package utf8
 import (
 	"testing"
 	stdlib "unicode/utf8"
+
+	"github.com/segmentio/asm/ascii"
 )
 
 func FuzzValid(f *testing.F) {
 	f.Fuzz(func(t *testing.T, data []byte) {
-		result := Valid(data)
-		ref := stdlib.Valid(data)
-		if result != ref {
-			t.Errorf("Valid(%q) = %v; want %v", data, result, ref)
+		vu, va := Validate(data)
+		ru := stdlib.Valid(data)
+		if ru != vu {
+			t.Errorf("Validate(%q) UTF8 = %v; want %v", data, vu, ru)
+		}
+		ra := ascii.Valid(data)
+		if ra != va {
+			t.Errorf("Validate(%q) ASCII = %v; want %v", data, va, ra)
 		}
 	})
 }
