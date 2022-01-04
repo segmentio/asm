b05fa38918c88ef1a475eeb7dc03a7180fb5d27b
Thomas Pelletier <thomas.pelletier@segment.com>
2022-01-04 09:51:54 -0500

    Change import names for fallback code

--- utf8/valid_default.go
+++ utf8/valid_default.go
@@ -4,17 +4,17 @@
 package utf8
 
 import (
-	"github.com/segmentio/asm/ascii"
-	stdlib "unicode/utf8"
+	segascii "github.com/segmentio/asm/ascii"
+	stdutf8 "unicode/utf8"
 )
 
 
 // Validate is a more precise version of Valid that also indicates whether the
 // input was valid ASCII.
 func Validate(p []byte) (utf8, ascii bool) {
-	valid := ascii.Valid(p)
+	valid := segascii.Valid(p)
 	if valid {
 		return true, true
 	}
-	return stdlib.Valid(p), false
+	return stdutf8.Valid(p), false
 }
