71f4c92d0128b819156611df7a5068d59662e164
Thomas Pelletier <thomas.pelletier@segment.com>
2022-01-04 11:27:20 -0500

    Don't run ascii.Valid

--- utf8/valid_support_amd64.go
+++ utf8/valid_support_amd64.go
@@ -5,15 +5,9 @@ package utf8
 
 import (
 	stdutf8 "unicode/utf8"
-
-	segascii "github.com/segmentio/asm/ascii"
 )
 
 func Validate(p []byte) (bool, bool) {
-	asciivalid := segascii.Valid(p)
-	if asciivalid {
-		return true, true
-	}
 	if len(p) < 32 {
 		return stdutf8.Valid(p), false
 	}
