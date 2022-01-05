ed0e17b22d91fc9e42e5e60e60730e73fc84166c
Thomas Pelletier <thomas.pelletier@segment.com>
2022-01-04 20:35:33 -0500

    Remove unused imports

--- utf8/valid_default.go
+++ utf8/valid_default.go
@@ -3,13 +3,6 @@
 
 package utf8
 
-import (
-	"unicode/utf8"
-
-	"github.com/segmentio/asm/ascii"
-)
-
-
 // Validate is a more precise version of Valid that also indicates whether the
 // input was valid ASCII.
 func Validate(p []byte) Validation {
