c66846544f3fd256ab14cfb4d204a43d6417b00f
Jeremy Larkin <jeremy.larkin@segment.com>
2022-01-14 22:09:34 +0000

    rewrite encode for 25% speedup

--- base64/encode_arm64.s
+++ base64/encode_arm64.s
@@ -1,86 +1,100 @@
 #include "textflag.h"
 
-#define Rdst R6
-#define Rsrc R5
-#define Rlen R1
-#define Rrd  R2
-#define Rwr  R3
-#define Rlut R0
-#define Ridx R0
+#define Rdst  R0
+#define Rsrc  R1
+#define Rlen  R2
+#define Rwr   R3
+#define Rrem  R4
+#define Rtmp  R5
+
+#define Vlut V0
+#define Vfld0 V6
+#define Vfld1 V7
+#define Vfld2 V8
+#define Vfld3 V9
+#define Vsrc0 V10
+#define Vsrc1 V11
+#define Vsrc2 V12
+#define Vr0a V13
+#define Vr1a V14
+#define Vr2a V15
+#define Vr3a V16
+#define Vr0b V17
+#define Vr1b V18
+#define Vr2b V19
+#define Vr3b V20
 
 // func encodeARM64(dst []byte, src []byte, lut *int8) (int, int)
 TEXT ·encodeARM64(SB),NOSPLIT,$0-72
+	// Load dst/src info
 	MOVD    dst_base+0(FP), Rdst
 	MOVD    src_base+24(FP), Rsrc
 	MOVD    src_len+32(FP), Rlen
-	MOVD    lut+48(FP), Rlut
-	VLD1    (Rlut), [V6.B16]
-	VMOVI   $51, V17.B16
-	VMOVI   $25, V16.B16
-	VMOVI   $63, V19.B16
-	VMOVI   $13, V7.B16
+	MOVD    lut+48(FP), Rtmp
+	VLD1    (Rtmp), [Vlut.B16]
 
+	MOVD    Rlen, Rrem
 	MOVD    Rdst, Rwr
-	MOVD    Rsrc, Rrd
+
+	VMOVI   $51, V1.B16
+	VMOVI   $26, V2.B16
+	VMOVI   $63, V3.B16
+	VMOVI   $13, V4.B16
 
 loop:
-	SUB     $48, Rlen, Rlen
-	VLD3.P  48(Rrd), [V25.B16, V26.B16, V27.B16]
-	VUSHR   $4, V26.B16, V4.B16
-	VSHL    $4, V25.B16, V22.B16
-	VUSHR   $2, V25.B16, V28.B16
-	VUSHR   $6, V27.B16, V18.B16
-	VSHL    $2, V26.B16, V21.B16
-	VORR    V4.B16, V22.B16, V22.B16
-	WORD    $0x6e3c3e04 //VCMHS   V28.B16, V16.B16, V4.B16
-	WORD    $0x6e312f85 //VUQSUB  V17.B16, V28.B16, V5.B16
-	VORR    V18.B16, V21.B16, V21.B16
-	VAND    V19.B16, V22.B16, V22.B16
-	VAND    V7.B16, V4.B16, V4.B16
-	VAND    V27.B16, V19.B16, V18.B16
-	VAND    V19.B16, V21.B16, V21.B16
-	WORD    $0x6e363e14 //VCMHS   V22.B16, V16.B16, V20.B16
-	VORR    V5.B16, V4.B16, V4.B16
-	WORD    $0x6e312ed7 //VUQSUB  V17.B16, V22.B16, V23.B16
-	VTBL    V4.B8, [V6.B16], V24.B8
-	WORD    $0x6e353e05 //VCMHS   V21.B16, V16.B16, V5.B16
-	VAND    V7.B16, V20.B16, V20.B16
-	VDUP    V24.D[0], V24
-	VDUP    V4.D[1], V4
-	VAND    V7.B16, V5.B16, V5.B16
-	VORR    V23.B16, V20.B16, V20.B16
-	VTBL    V4.B8, [V6.B16], V4.B8
-	VTBL    V20.B8, [V6.B16], V23.B8
-	VMOV    V4.D[0], V24.D[1]
-	WORD    $0x6e312eb9 //VUQSUB  V17.B16, V21.B16, V25.B16
-	VDUP    V23.D[0], V23
-	WORD    $0x6e323e04 //VCMHS   V18.B16, V16.B16, V4.B16
-	VADD    V28.B16, V24.B16, V0.B16
-	VORR    V25.B16, V5.B16, V5.B16
-	VDUP    V20.D[1], V20
-	VAND    V7.B16, V4.B16, V4.B16
-	VTBL    V20.B8, [V6.B16], V20.B8
-	WORD    $0x6e312e58 //VUQSUB  V17.B16, V18.B16, V24.B16
-	VMOV    V20.D[0], V23.D[1]
-	VTBL    V5.B8, [V6.B16], V20.B8
-	VDUP    V20.D[0], V20
-	VORR    V24.B16, V4.B16, V4.B16
-	VDUP    V5.D[1], V5
-	VTBL    V5.B8, [V6.B16], V5.B8
-	VMOV    V5.D[0], V20.D[1]
-	VTBL    V4.B8, [V6.B16], V5.B8
-	VDUP    V5.D[0], V5
-	VDUP    V4.D[1], V4
-	VTBL    V4.B8, [V6.B16], V4.B8
-	VADD    V22.B16, V23.B16, V1.B16
-	VMOV    V4.D[0], V5.D[1]
-	VADD    V21.B16, V20.B16, V2.B16
-	VADD    V18.B16, V5.B16, V3.B16
-	VST4.P  [V0.B16, V1.B16, V2.B16, V3.B16], 64(Rwr)
-	CMP     $48, Rlen
-	BHS     loop
-	SUB     Rdst, Rwr, R0
-	SUB     Rsrc, Rrd, R1
-	MOVD    R0, ret+56(FP)
-	MOVD    R1, ret1+64(FP)
+	VLD3.P  48(Rsrc), [Vsrc0.B16, Vsrc1.B16, Vsrc2.B16]
+
+	// Split 3 source blocks into 4 lookup inputs
+	VUSHR   $2, Vsrc0.B16, Vfld0.B16
+	VUSHR   $4, Vsrc1.B16, Vfld1.B16
+	VUSHR   $6, Vsrc2.B16, Vfld2.B16
+	VSHL    $4, Vsrc0.B16, Vsrc0.B16
+	VSHL    $2, Vsrc1.B16, Vsrc1.B16
+	VORR    Vsrc0.B16, Vfld1.B16, Vfld1.B16
+	VORR    Vsrc1.B16, Vfld2.B16, Vfld2.B16
+	VAND    V3.B16, Vfld1.B16, Vfld1.B16
+	VAND    V3.B16, Vfld2.B16, Vfld2.B16
+	VAND    V3.B16, Vsrc2.B16, Vfld3.B16
+
+	// if v < 51 { v = 0 } else { v = v - 51 }
+	WORD    $0x6e212ccd // VUQSUB  V1.B16, Vfld0.B16, Vr0a.B16
+	WORD    $0x6e212cee // VUQSUB  V1.B16, Vfld1.B16, Vr1a.B16
+	WORD    $0x6e212d0f // VUQSUB  V1.B16, Vfld2.B16, Vr2a.B16
+	WORD    $0x6e212d30 // VUQSUB  V1.B16, Vfld3.B16, Vr3a.B16
+
+	// Split lookup ranges 0..25 -> 0 and 26..51 -> 13
+	WORD    $0x4e263451 // VCMGT   V2.B16, Vfld0.B16, Vr0b.B16
+	VAND    V4.B16, Vr0b.B16, Vr0b.B16
+	VORR    Vr0b.B16, Vr0a.B16, Vr0a.B16
+	WORD    $0x4e273452 // VCMGT   V2.B16, Vfld1.B16, Vr1b.B16
+	VAND    V4.B16, Vr1b.B16, Vr1b.B16
+	VORR    Vr1b.B16, Vr1a.B16, Vr1a.B16
+	WORD    $0x4e283453 // VCMGT   V2.B16, Vfld2.B16, Vr2b.B16
+	VAND    V4.B16, Vr2b.B16, Vr2b.B16
+	VORR    Vr2b.B16, Vr2a.B16, Vr2a.B16
+	WORD    $0x4e293454 // VCMGT   V2.B16, Vfld3.B16, Vr3b.B16
+	VAND    V4.B16, Vr3b.B16, Vr3b.B16
+	VORR    Vr3b.B16, Vr3a.B16, Vr3a.B16
+
+	// Add result of lookup table to each field
+	VTBL    Vr0a.B16, [Vlut.B16], Vr0a.B16
+	VADD    Vr0a.B16, Vfld0.B16, Vfld0.B16
+	VTBL    Vr1a.B16, [Vlut.B16], Vr1a.B16
+	VADD    Vr1a.B16, Vfld1.B16, Vfld1.B16
+	VTBL    Vr2a.B16, [Vlut.B16], Vr2a.B16
+	VADD    Vr2a.B16, Vfld2.B16, Vfld2.B16
+	VTBL    Vr3a.B16, [Vlut.B16], Vr3a.B16
+	VADD    Vr3a.B16, Vfld3.B16, Vfld3.B16
+
+	VST4.P  [Vfld0.B16, Vfld1.B16, Vfld2.B16, Vfld3.B16], 64(Rwr)
+	SUB     $48, Rrem
+	CMP     $48, Rrem
+	BGE     loop
+
+done:
+	SUB     Rdst, Rwr
+	SUB     Rrem, Rlen
+	MOVD    Rwr, ret+56(FP)
+	MOVD    Rlen, ret1+64(FP)
 	RET
+
